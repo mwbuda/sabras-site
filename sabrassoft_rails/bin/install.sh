59b1820f10bd716d6d8988de5e82668e32b2ad78
mwbuda <mbuda@sabrassoft.com>
2015-06-18 15:14:25 -0400

    don't do update in install script

--- sabrassoft_rails/bin/install.sh
+++ sabrassoft_rails/bin/install.sh
@@ -2,6 +2,6 @@
 
 cp ~/.env ~/sabrassoft_rails
 export GEM_HOME=~/ruby/gems
-~/sabrassoft_rails/bin/bundle update
+#~/sabrassoft_rails/bin/bundle update
 ~/sabrassoft_rails/bin/bundle install
 touch ~/sabrassoft_rails/tmp/restart.txt
